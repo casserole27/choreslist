2725809cae7b6ac99f70c8b50662d0536390dfd0
C Lewis <casserole.lewis@gmail.com>
2023-03-23 08:34:15 -0400

    JS - update event listeners
    Add button functionality, update logic to getting chores from an array

--- index.js
+++ index.js
@@ -7,21 +7,45 @@
 
 /****** VARIABLES ******/
 
+let myChores = [];
 const choresInput = document.getElementById("choresinput")
 let choresList = document.getElementById("choreslist");
 
-const chore = choresInput.value;
-console.log(chore);
+const arrowBtn = document.getElementById("arrow-btn");
+const xBtn = document.getElementById("x-btn");
 
 /****** FUNCTIONS ******/
 
-function renderChores() {
-    choresList += `
-        <li class="chore"
-            chore
+function getChores() {
+    //! Have to do an array for more than one value
+    myChores.push(choresInput.value);
+    choresInput.value = "";
+    renderChores(myChores);
+}
+
+function renderChores(chores) {
+    
+   let listItems = ""
+    
+   chores.forEach(chore => {
+        listItems += `
+        <li class="chore">
+            ${chore}
         </li>    
-    `
+`
+   });
+
+    choresList.innerHTML = listItems;
+}
+
+function removeChores() {
+    myChores = [];
+    choresInput.value = "";
+    renderChores(myChores);
 }
 
 
 /****** EVENT LISTENERS ******/
+
+arrowBtn.addEventListener("click", getChores);
+xBtn.addEventListener("click", removeChores);
